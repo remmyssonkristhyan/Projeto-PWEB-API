d1c2d76296fa054f44df4c07786e642904b607cb
remmyssonkristhyan <rkristhyan@gmail.com>
2022-10-20 15:21:17 -0300

    changes to read .env variables

--- src/database/index.js
+++ src/database/index.js
@@ -1,20 +1,28 @@
+const enviroment = require('dotenv').config();
 const mysql = require("mysql2/promise");
-require('dotenv').config();
+
+
+console.log(enviroment);
 
 const connectToMysql = async () => {
     if (global.connection && global.connection.state !== 'disconnected')
         return global.connection;
 
     const config = {
-        host: process.env.HOST,
-        user: process.env.USERNAME,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE
+        host: enviroment.parsed.HOST,
+        user: enviroment.parsed.USER,
+        password: enviroment.parsed.PASSWORD,
+        database: enviroment.parsed.DATABASE
     }
 
     const connection = await mysql.createConnection(config);
     console.log('Conexão realizada com sucesso');
     global.connection = connection;
+
+    // console.log('Host: ', process.env.HOST);
+    // console.log('User: ', process.env.USER);
+    // console.log('Password: ', process.env.PASSWORD);
+    // console.log('Database: ', process.env.DATABASE);
 }
 
 connectToMysql();
